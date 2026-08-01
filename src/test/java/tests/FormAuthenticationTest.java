86906863e13ba3c98eec4914f69cdb214bd081cb
n.shinov <n.shinov@a1qa.com>
2026-08-01 15:56:05 +0200

    userdata fix

--- src/test/java/tests/FormAuthenticationTest.java
+++ src/test/java/tests/FormAuthenticationTest.java
@@ -3,26 +3,27 @@ package tests;
 import constants.MainPageNavigation;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.FormAuthenticationPage;
+import pages.LoginPage;
+import utils.TestDataReader;
 import pages.SecureAreaPage;
 
 public class FormAuthenticationTest extends BaseTest {
 
-    private final FormAuthenticationPage formAuthenticationPage = new FormAuthenticationPage();
+    private final LoginPage loginPage = new LoginPage();
     private final SecureAreaPage secureAreaPage = new SecureAreaPage();
 
     @Test
     public void formAuthenticationTest() {
         mainPage.clickNavigationLink(MainPageNavigation.FORM_AUTHENTICATION);
-        Assert.assertTrue(formAuthenticationPage.state().waitForDisplayed(), "Form Authentication page is not open");
+        Assert.assertTrue(loginPage.state().waitForDisplayed(), "Form Authentication page is not open");
         
-        formAuthenticationPage.login("tomsmith", "SuperSecretPassword!");
+        loginPage.login(TestDataReader.getUserData().getUsername(), TestDataReader.getUserData().getPassword());
         
         Assert.assertTrue(secureAreaPage.state().waitForDisplayed(), "Secure area page is not open");
         Assert.assertTrue(secureAreaPage.getSuccessMessageText().contains("You logged into a secure area!"), "Successful login message is not displayed");
         
         secureAreaPage.clickLogout();
         
-        Assert.assertTrue(formAuthenticationPage.state().waitForDisplayed(), "Form Authentication page is not open after logout");
+        Assert.assertTrue(loginPage.state().waitForDisplayed(), "Form Authentication page is not open after logout");
     }
 }
